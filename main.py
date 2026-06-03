c919a12a8c0fef3615c80d6d24c2f184ca3b2ef0
lvmxn <lukanindan@gmail.com>
2026-06-03 22:59:15 +0500

    feat: added mean statistic for agent's class and updated graphs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random 
 import matplotlib.pyplot as plt
+import numpy as np
 
 agent_count = 5
 runs_count = 10
@@ -55,6 +56,11 @@ class Collector(Agent):
             Collector.collector_count += 1
             newborns.append(Collector(f"c{Collector.collector_count}"))
 
+
+all_runs_alive_all=[]
+all_runs_alive_hunters=[]
+all_runs_alive_collectors=[]
+
 for run in range(runs_count):
     agents = []
     for i in range(agent_count):
@@ -67,7 +73,9 @@ for run in range(runs_count):
     newborn_count_c = 0
     newborn_count_h = 0
     days = []
-    alive=[]
+    alive_all=[]
+    alive_hunters=[]
+    alive_collectors=[]
 
     for day in range(days_count):
         newborns = []
@@ -80,13 +88,28 @@ for run in range(runs_count):
             if agent.alive:
                 agent.breeding(newborns)
         days.append(day+1)
-        alive.append(len([c for c in agents if c.alive]))
+        alive_all.append(len([c for c in agents if c.alive]))
+        alive_hunters.append(len([c for c in agents if 'h' in c.name and c.alive]))
+        alive_collectors.append(len([c for c in agents if 'c' in c.name and c.alive]))
         agents.extend(newborns)
         newborn_count += len(newborns)
         newborn_count_c += len([c for c in newborns if 'c' in c.name])
         newborn_count_h += len([c for c in newborns if 'h' in c.name])
-        plt.plot(days,alive)
+
+    all_runs_alive_all.append(alive_all)
+    all_runs_alive_hunters.append(alive_hunters)
+    all_runs_alive_collectors.append(alive_collectors)
 
 
+matrix_all = np.array(all_runs_alive_all)
+matrix_hunters = np.array(all_runs_alive_hunters)
+matrix_collectors = np.array(all_runs_alive_collectors)
+average_alive_all = np.mean(matrix_all, axis=0)
+average_alive_hunters = np.mean(matrix_hunters, axis=0)
+average_alive_collectors = np.mean(matrix_collectors, axis=0)
+plt.plot(days, average_alive_all, color="blue", linewidth=3, label="mean alive all")
+plt.plot(days, average_alive_hunters, color="red", linewidth=2, label="mean alive hunters")
+plt.plot(days, average_alive_collectors, color="green", linewidth=2, label="mean alive collectors")
 plt.grid(True)
+plt.legend()
 plt.show()
